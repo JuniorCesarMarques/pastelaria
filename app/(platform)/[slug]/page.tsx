57fe1b6db41357dfc2ecce64fc4860a70caaa860
JuniorCesarMarques <jcesargithub@gmail.com>
2026-08-12 22:02:03 -0300

    incremental 2

--- app/(platform)/[slug]/page.tsx
+++ app/(platform)/[slug]/page.tsx
@@ -25,68 +25,68 @@ export default function Home() {
 
   const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
 
-  // const onSubmit = async (data: ProductForm) => {
-  //   if (selectedProduct) {
-  //     try {
-  //       const imgFromData =
-  //         data.imagem instanceof FileList ? data.imagem?.[0] : data.imagem;
+  const onSubmit = async (data: ProductForm) => {
+    if (selectedProduct) {
+      try {
+        const imgFromData =
+          data.imagem instanceof FileList ? data.imagem?.[0] : data.imagem;
 
-  //       const imagem =
-  //         imgFromData instanceof File
-  //           ? await uploadImage(
-  //               imgFromData,
-  //               "store-platform-assets",
-  //               `${slug}/products`,
-  //             )
-  //           : imgFromData;
+        const imagem =
+          imgFromData instanceof File
+            ? await uploadImage(
+                imgFromData,
+                "store-platform-assets",
+                `${slug}/products`,
+              )
+            : imgFromData;
 
-  //       const isNewImage = !(typeof imagem === "string" || imagem === null)
+        const isNewImage = !(typeof imagem === "string" || imagem === null)
 
-  //       const updatedProduct = {
-  //         ...data,
-  //         id: selectedProduct.id,
-  //         imagem:
-  //           isNewImage
-  //             ? imagem.publicUrl
-  //             : imagem,
-  //       };
+        const updatedProduct = {
+          ...data,
+          id: selectedProduct.id,
+          imagem:
+            isNewImage
+              ? imagem.publicUrl
+              : imagem,
+        };
 
-  //       const res = await fetch("/api/produtos", {
-  //         method: "PUT",
-  //         headers: {
-  //           "Content-Type": "application/json",
-  //         },
-  //         body: JSON.stringify(updatedProduct),
-  //       });
+        const res = await fetch("/api/produtos", {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(updatedProduct),
+        });
 
-  //       const result = await res.json();
+        const result = await res.json();
 
-  //       if (!res.ok) {
-  //         if (isNewImage) {
-  //           await deleteImage(imagem.publicUrl);
-  //         }
+        if (!res.ok) {
+          if (isNewImage) {
+            await deleteImage(imagem.publicUrl);
+          }
 
-  //         throw new Error(result.message);
-  //       }
+          throw new Error(result.message);
+        }
 
-  //       if (isNewImage && selectedProduct.imagem) {
-  //         await deleteImage(selectedProduct.imagem);
-  //       }
+        if (isNewImage && selectedProduct.imagem) {
+          await deleteImage(selectedProduct.imagem);
+        }
 
-  //       setProducts((prev) =>
-  //         prev.map((p) => (p.id === selectedProduct.id ? updatedProduct : p)),
-  //       );
+        setProducts((prev) =>
+          prev.map((p) => (p.id === selectedProduct.id ? updatedProduct : p)),
+        );
 
-  //       setSelectedProduct(null);
-  //       setModalState(false);
-  //       toast.success(result.message);
-  //     } catch (err) {
-  //       toast.error(err as string);
-  //     }
-  //   } else {
-  //     const res = await fetch(``);
-  //   }
-  // };
+        setSelectedProduct(null);
+        setModalState(false);
+        toast.success(result.message);
+      } catch (err) {
+        toast.error(err as string);
+      }
+    } else {
+      const res = await fetch(``);
+    }
+  };
 
   // useEffect(() => {
   //   (async () => {
